4acd0f9f220fbd0ab5c743aa096ea8ec52e1bd82
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2025-02-09 13:27:21 +0530

    feat: refactor Redis client usage to a single instance

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,14 @@ import "dotenv/config";
 
 const app = new Hono();
 
+// Maximum number of keys that can be stored in the cache
 const MAX_SIZE = 10;
+
+// Create a Redis client
+if (!process.env.REDIS_CONNECTION_URL) {
+  throw new Error("REDIS_CONNECTION_URL is required");
+}
+const redisClient = new Redis(process.env.REDIS_CONNECTION_URL);
 
 // GET / -> Server is running 🔥
 app.get("/", (c) => {
@@ -14,15 +21,10 @@ app.get("/", (c) => {
 
 // POST /cache { key: string, value: string }
 app.post("/cache", async (c) => {
-  if (!process.env.REDIS_CONNECTION_URL) {
-    return c.text("REDIS_CONNECTION_URL is required", 500);
-  }
-  const redis = new Redis(process.env.REDIS_CONNECTION_URL);
-
   const { key, value } = await c.req.json();
 
   // Check if the cache limit is reached
-  const size = await redis.dbsize();
+  const size = await redisClient.dbsize();
   if (size >= MAX_SIZE) {
     return c.text(
       `Cache limit reached. Cannot store more than ${MAX_SIZE} keys.`,
@@ -31,27 +33,22 @@ app.post("/cache", async (c) => {
   }
 
   // Check if the key already exists
-  const exists = await redis.exists(key);
+  const exists = await redisClient.exists(key);
   if (exists === 1) {
     return c.text("Key already exists.", 400);
   }
 
   // Store the key-value pair and return the response
-  await redis.set(key, value);
+  await redisClient.set(key, value);
   return c.text(`Stored {${key} : ${value}} successfully.`);
 });
 
 // GET /cache/:key -> Value: string
 app.get("/cache/:key", async (c) => {
-  if (!process.env.REDIS_CONNECTION_URL) {
-    return c.text("REDIS_CONNECTION_URL is required", 500);
-  }
-  const redis = new Redis(process.env.REDIS_CONNECTION_URL);
-
   const key = c.req.param("key");
 
   // Get the value from the cache
-  const data = await redis.get(key);
+  const data = await redisClient.get(key);
 
   // If the key is not found, return 404
   if (data === null) {
@@ -64,15 +61,10 @@ app.get("/cache/:key", async (c) => {
 
 // DELETE /cache/:key -> Deleted key: string
 app.delete("/cache/:key", async (c) => {
-  if (!process.env.REDIS_CONNECTION_URL) {
-    return c.text("REDIS_CONNECTION_URL is required", 500);
-  }
-  const redis = new Redis(process.env.REDIS_CONNECTION_URL);
-
   const key = c.req.param("key");
 
   // Delete the key from the cache
-  await redis.del(key);
+  await redisClient.del(key);
   return c.text(`Deleted key: ${key}`);
 });
 
